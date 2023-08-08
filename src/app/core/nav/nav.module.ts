0738ea163cde3f09a8e0edd7912cc94fa974ec5c
jprivet-dev <jean.philippe.rivet@gmail.com>
2023-08-08 14:29:07 +0200

    feat(nav): create base of the navigation

--- src/app/core/nav/nav.module.ts
+++ src/app/core/nav/nav.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavComponent } from './nav.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [NavComponent],
   exports: [NavComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
 })
 export class NavModule {}
