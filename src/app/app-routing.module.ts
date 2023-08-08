f3af076bb8df0125b883553da13bbf984ade538c
jprivet-dev <jean.philippe.rivet@gmail.com>
2023-08-08 10:23:07 +0200

    feat(routing): update app & resistor

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'resistor', pathMatch: 'full' },
+  {
+    path: 'resistor',
+    loadChildren: () =>
+      import('src/app/resistor/resistor.module').then((m) => m.ResistorModule),
+  },
+  { path: '**', component: NotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
