9864b7268a1ebe1089e9eeec93fbf32cebc1d846
jprivet-dev <jean.philippe.rivet@gmail.com>
2023-08-08 14:48:23 +0200

    feat(nav): activate collapse behavior

--- src/app/core/nav/nav.module.ts
+++ src/app/core/nav/nav.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavComponent } from './nav.component';
 import { RouterModule } from '@angular/router';
+import {
+  NgbCollapseModule,
+  NgbDropdownModule,
+} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [NavComponent],
   exports: [NavComponent],
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, NgbDropdownModule, NgbCollapseModule],
 })
 export class NavModule {}
