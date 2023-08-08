ba291a95dcce9079407c75082ef7d5d50d5010c6
jprivet-dev <jean.philippe.rivet@gmail.com>
2023-08-08 10:01:50 +0200

    feat(resistor): generate base of module & component

---
+++ src/app/resistor/resistor.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-resistor',
+  templateUrl: './resistor.component.html',
+  styleUrls: ['./resistor.component.scss'],
+})
+export class ResistorComponent {}
---
+++ src/app/resistor/resistor.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ResistorComponent } from './resistor.component';
+
+@NgModule({
+  declarations: [ResistorComponent],
+  imports: [CommonModule],
+})
+export class ResistorModule {}
