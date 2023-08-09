f52754828eafb7e35da3fece42183f29e69d29f4
jprivet-dev <jean.philippe.rivet@gmail.com>
2023-08-09 10:59:08 +0200

    build(i18n): install transloco

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { CoreModule } from './core/core.module';
+import { HttpClientModule } from '@angular/common/http';
+import { TranslocoRootModule } from './transloco-root.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, CoreModule, AppRoutingModule, NgbModule],
+  imports: [BrowserModule, CoreModule, AppRoutingModule, NgbModule, HttpClientModule, TranslocoRootModule],
   providers: [],
   bootstrap: [AppComponent],
 })
