56be527c2128a12fc6f5effac529823b89bff6f4
jprivet-dev <jean.philippe.rivet@gmail.com>
2023-08-16 17:52:59 +0200

    feat(resistor): use tabs for characteristics and charts

--- src/app/resistor/resistor-data.component.ts
+++ src/app/resistor/resistor-data.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-resistor-data',
   templateUrl: './resistor-data.component.html',
-  styleUrls: ['./resistor-data.component.scss']
+  styleUrls: ['./resistor-data.component.scss'],
 })
 export class ResistorDataComponent {
-
+  activeTabId = 1;
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormatPipe } from './format.pipe';
 import { ResistorColorDirective } from './resistor-color.directive';
+import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [FormatPipe, ResistorColorDirective],
   imports: [CommonModule],
-  exports: [FormatPipe, ResistorColorDirective],
+  exports: [FormatPipe, ResistorColorDirective, NgbNavModule],
 })
 export class SharedModule {}
