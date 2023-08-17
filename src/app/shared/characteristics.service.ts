b11dc5ecd4fd496408c01f9155e7950a91396d8b
jprivet-dev <jean.philippe.rivet@gmail.com>
2023-08-17 14:16:59 +0200

    feat(resistor.characteristics): create base of service

---
+++ src/app/shared/characteristics.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { Resistor } from '../resistor/resistor.model';
+import { resistorConfig } from '../resistor/resistor.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CharacteristicsService {
+  private resistorConfig = resistorConfig;
+
+  calculateResistance(resistor: Resistor): number {
+    const digit1Color = resistor.digit1.color;
+    const digit1: number = this.resistorConfig.digit1[digit1Color] ?? 0;
+
+    const digit2Color = resistor.digit2.color;
+    const digit2: number = this.resistorConfig.digit2[digit2Color] ?? 0;
+
+    const digit3Color = resistor.digit3.color;
+    const digit3: number = this.resistorConfig.digit3[digit3Color] ?? 0;
+
+    const multiplierColor = resistor.multiplier.color;
+    const multiplier: number =
+      this.resistorConfig.multiplier[multiplierColor] ?? 0;
+
+    return resistor.bandsCount <= 4
+      ? (digit1 * 10 + digit2) * multiplier
+      : (digit1 * 100 + digit2 * 10 + digit3) * multiplier;
+  }
+}
