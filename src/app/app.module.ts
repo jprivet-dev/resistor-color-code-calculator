689e2abac1865f08173b1a09ce3ac8433fac7759
jprivet-dev <jean.philippe.rivet@gmail.com>
2023-08-18 07:09:17 +0200

    build(store): install devtools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { CoreModule } from '@core/core.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
@@ -12,6 +12,7 @@ import { characteristicsReducer } from './resistor/state/resistor.reducer';
 import { TranslocoRootModule } from './transloco-root.module';
 import { EffectsModule } from '@ngrx/effects';
 import { ResistorEffects } from './resistor/state/resistor.effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 @NgModule({
   declarations: [AppComponent],
@@ -28,6 +29,7 @@ import { ResistorEffects } from './resistor/state/resistor.effects';
       characteristics: characteristicsReducer,
     }),
     EffectsModule.forRoot([ResistorEffects]),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() }),
   ],
   providers: [],
   bootstrap: [AppComponent],
