dca7a1b77fa06fb8afb86cc18dbd85bb36402b0f
jprivet-dev <jean.philippe.rivet@gmail.com>
2023-08-19 16:15:33 +0200

    refactor(app.state): remove unused code

--- src/app/state/app.state.ts
+++
@@ -1,7 +0,0 @@
-import { Resistor } from '../resistor/resistor.model';
-
-export interface AppState {
-  resistor: Readonly<Resistor>;
-}
-
-// export const selectResistor = (state: AppState) => state.resistor;
