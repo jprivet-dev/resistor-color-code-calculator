e103f70a338cf872714d5ffa33c895667f3a4a1f
jprivet-dev <jean.philippe.rivet@gmail.com>
2023-08-22 00:35:33 +0200

    feat(about): complete fr & en page

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { LangService } from '@core/lang/lang.service';
+import { TranslocoModule } from '@ngneat/transloco';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TranslocoModule],
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  styleUrls: ['./about.component.scss'],
 })
 export class AboutComponent {
-
+  private langService = inject(LangService);
+  readonly langLabel$ = this.langService.langLabel$;
 }
